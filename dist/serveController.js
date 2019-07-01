c1dfa4371991d87ebb13dbf9324b663855130b2b
Bruce Wong <dbrucewong@gmail.com>
2019-07-01 11:48:33 -0700

    More tests setup

--- dist/serveController.js
+++ dist/serveController.js
@@ -1,6 +1,10 @@
 "use strict";
 module.exports = () => ({
-    createHandler: (dir, useStatic, timeout) => { },
+    createHandler: (dir, useStatic, timeout) => {
+        return function (req, res, next) {
+            return;
+        };
+    },
     createCallback: (res) => { },
     promiseHandler: (promise, cb) => { },
 });
--- src/serve/serveController.ts
+++ src/serve/serveController.ts
@@ -1,5 +1,13 @@
 module.exports = () => ({
-  createHandler: (dir: string, useStatic: boolean, timeout: number) => { },
-  createCallback: (res: { headers: string; statusCode: number }) => { },
-  promiseHandler: (promise: Function, cb: Function) => { },
-}); 
+  createHandler: (dir: string, useStatic: boolean, timeout: number) => {
+    return function(
+      req: Express.Request,
+      res: Express.Response,
+      next: Function
+    ) {
+      return;
+    };
+  },
+  createCallback: (res: { headers: string; statusCode: number }) => {},
+  promiseHandler: (promise: Function, cb: Function) => {},
+});
