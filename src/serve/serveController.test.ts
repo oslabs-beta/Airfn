153cc29f6867648cbb6b97ac21c27d9180531df4
Bruce Wong <dbrucewong@gmail.com>
2019-07-01 21:25:19 -0700

    MVP serve functionality

--- src/serve/serveController.test.ts
+++ src/serve/serveController.test.ts
@@ -1,15 +1,15 @@
 const {
   createHandler,
   createCallback,
-  promiseHandler
-} = require("./serveController")();
+  promiseHandler,
+} = require('./serveController')();
 
 /* Sets up test mocks for Express request and response objects */
 function setup() {
   const req = {
     body: {},
-    path: "",
-    url: ""
+    path: '',
+    url: '',
   };
   const res = {
     locals: {
@@ -18,7 +18,7 @@ function setup() {
     },
     statusCode: null,
     body: '',
-    headers: {}
+    headers: {},
   };
   const next = jest.fn();
   Object.assign(res, {
@@ -36,56 +36,56 @@ function setup() {
       function send(this: object) {
         return this;
       }.bind(res)
-    )
+    ),
   });
   return { req, res, next };
 }
 
-describe("createHandler", () => {
-  test("Should have proper error object in res.locals if requiring function module fails", async () => {
+describe('createHandler', () => {
+  test('Should have proper error object in res.locals if requiring function module fails', async () => {
     const { req, res, next } = setup();
-    req.path = "/helloasync";
-    const dir = "/functions";
+    req.path = '/helloasync';
+    const dir = '/functions';
     const useStatic = false;
     const timeout = 5;
     const errorObj = {
       code: 500,
-      type: "Server",
-      message: "Loading function failed"
+      type: 'Server',
+      message: 'Loading function failed',
     };
     await createHandler(dir, useStatic, timeout)(req, res, () => {
       expect(res.locals.error).toEqual(errorObj);
     });
   });
 
-  test("Should have a proper error object in res.locals if lambda is not invoked before timeout", async () => {
+  test('Should have a proper error object in res.locals if lambda is not invoked before timeout', async () => {
     const { req, res, next } = setup();
-    req.path = "/helloasync";
-    const dir = "/functions";
+    req.path = '/helloasync';
+    const dir = '/functions';
     const useStatic = false;
     const timeout = 5;
     const errorObj = {
       code: 400,
-      type: "Client",
-      message: "Failed to invoke function before timeout"
+      type: 'Client',
+      message: 'Failed to invoke function before timeout',
     };
     await createHandler(dir, useStatic, timeout)(req, res, () => {
       expect(res.locals.error).not.toEqual(errorObj);
     });
   });
 
-  test("Should have a proper lambdaResponse object in res.locals", async () => {
+  test('Should have a proper lambdaResponse object in res.locals', async () => {
     const { req, res, next } = setup();
-    req.path = "/helloasync";
-    req.url = "http://localhost:9000/helloasync";
-    const dir = "/functions";
+    req.path = '/helloasync';
+    req.url = 'http://localhost:9000/helloasync';
+    const dir = '/functions';
     const useStatic = false;
     const timeout = 5;
     const lambdaResponse = [
       {
         statusCode: 200,
-        body: "Hello, World"
-      }
+        body: 'Hello, World',
+      },
     ];
     await createHandler(dir, useStatic, timeout)(req, res, () => {
       expect(res.locals.lambdaResponse).not.toEqual(lambdaResponse);
@@ -107,7 +107,7 @@ describe('createCallback', () => {
 
   test('Should return a function', async () => {
     const res = {};
-    await expect(typeof createCallback(res)).toEqual('function')
+    await expect(typeof createCallback(res)).toEqual('function');
   });
 
   test('Returned callback should be able to handle errors', async () => {
@@ -121,9 +121,11 @@ describe('createCallback', () => {
         'Why should you never trust a pig with a ' +
         "secret? Because it's bound to squeal.",
     };
-    const returnFunc = await createCallback(res)
-    expect(returnFunc(new Error('this is an error'), null)).toBeInstanceOf(Error)
-  })
+    const returnFunc = await createCallback(res);
+    expect(returnFunc(new Error('this is an error'), null)).toBeInstanceOf(
+      Error
+    );
+  });
 
   test('Callback should set proper response object with status code, headers, and body', async () => {
     const { res } = setup();
@@ -131,13 +133,12 @@ describe('createCallback', () => {
       statusCode: 200,
       headers: {
         header1: 'Facebook',
-        header2: 'Google'
+        header2: 'Google',
       },
       body: 'Hello, World',
     };
     await createCallback(res)(null, lambdaResponse);
     await expect(res).toMatchObject(lambdaResponse);
-  })
-
+  });
 });
-describe('promiseHandler', () => { });
+describe('promiseHandler', () => {});
--- src/serve/serveController.ts
+++ src/serve/serveController.ts
@@ -1,47 +1,60 @@
-import express from "express";
-import path from "path";
-import queryString from "querystring";
+import { Request, Response } from 'express';
 
-const app: Express.Application = express();
-module.exports = () => ({
-  createHandler: (dir: string, useStatic: boolean, timeout: number) => {
-    return function (
-      req: Express.Request,
-      res: Express.Response,
-      next: Function
-    ) {
-      const fn: string = req.path.split("/").filter(name => name)[0];
-      const joinModPath = path.join(process.cwd(), dir, fn);
-      const handler = require(joinModPath);
-      const lambdaReq = {
-        path: req.path,
-        httpMethod: req.method,
-        queryStringParameters: queryString.parse(req.url.split(/\?(.+)/)[1]),
-        headers: req.headers,
-        body: req.body
-      };
-
-      const promise = handler.handler(lambdaReq, null, null);
-      Promise.all([promise]).then(result => res.send(result));
-      return;
+// TODO: Proper TypeScript types for modules
+export default (path: { join: Function }, queryString: { parse: Function }) => (
+  dir: string,
+  useStatic: boolean,
+  timeout: number
+) => {
+  return function(req: Request, res: Response, next: Function) {
+    const fn: string = req.path.split('/').filter(name => name)[0];
+    const joinModPath = path.join(process.cwd(), dir, fn);
+    const handler = require(joinModPath);
+    const lambdaReq = {
+      path: req.path,
+      httpMethod: req.method,
+      queryStringParameters: queryString.parse(req.url.split(/\?(.+)/)[1]),
+      headers: req.headers,
+      body: req.body,
     };
-  },
-  //headers are for now any.. will find bette way later
-  createCallback: (res: { body: string; statusCode: number; headers: any }) => {
-    // res.body = 'sadf';
-    return function callback(err: Error, lambdaResponse: any) {
-      // res.body = 'sadf';
-      // console.log(lambdaResponse);
-      if (err) return err
-      res.statusCode = lambdaResponse.statusCode;
-      for (let key in lambdaResponse.headers) {
-        res.headers[key] = lambdaResponse.headers[key];
-      }
+    const callback = createCallback(res);
+    const promise = handler.handler(lambdaReq, null, callback);
+    Promise.all([promisifyHandler(promise, callback)]) // TODO: Implement promise with timeout
+      .then(() => {
+        return next();
+      })
+      .catch(err => {
+        throw err;
+      });
+  };
+};
 
-      if (lambdaResponse.body) {
-        res.body = lambdaResponse.body;
-      }
+function createCallback(res: Response) {
+  return function callback(err: Error | null, lambdaRes: any) {
+    if (err) return err; // TODO: Proper error handling
+    res.statusCode = lambdaRes.statusCode;
+    for (let key in lambdaRes.headers) {
+      res.setHeader(key, lambdaRes.headers[key]);
     }
-  },
-  promiseHandler: (promise: Function, cb: Function) => { },
-});
+    if (lambdaRes.body) {
+      res.write(lambdaRes.body);
+    }
+  };
+}
+
+function promisifyHandler(promise: { then: Function }, callback: Function) {
+  if (
+    !promise ||
+    typeof promise.then !== 'function' ||
+    typeof callback !== 'function'
+  )
+    return;
+
+  return promise
+    .then((data: object) => {
+      callback(null, data);
+    })
+    .catch((err: Error | null) => {
+      callback(err, null);
+    });
+}
