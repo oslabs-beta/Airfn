5052ae2bf217b3f742faebf624634c91e0cf0ec4
dbrucewong <dbrucewong@gmail.com>
2019-07-02 22:53:12 -0700

    CLI Build build MVP functionality working (#6)
    * MVP serve functionality
    
    * CLI Build build MVP functionality working

--- src/serve/serve.ts
+++ src/serve/serve.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import queryString from 'querystring';
 import bodyParser from 'body-parser';
 const createHandler = serve(path, queryString);
+import run from '../build/build';
 
 const app: express.Application = express();
 app.use(bodyParser.json());
@@ -11,6 +12,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const DEFAULT_DIR: string = 'functions';
 const PORT = 9000;
+
+const dummyConfigs = {
+  userWebpackConfig: true,
+  userBabelrc: true,
+};
+run('functions', dummyConfigs)
+  .then(function(stats: any) {
+    console.log(stats.toString({ color: true }));
+  })
+  .catch(function(err: any) {
+    console.error(err);
+    process.exit(1);
+  });
 
 app.get('/favicon.ico', function(req, res) {
   return res.status(204).end();
